7a2c56b3c7c122d45fdcc8e517f6ee2a61a698e9
debutler266 <32465329+debutler266@users.noreply.github.com>
2020-11-17 12:59:10 -0500

    submit is successfully being blocked in js by adding event listener and input values are rendering to console on submit

--- main.js
+++ main.js
@@ -5,5 +5,11 @@ submitForm);
 
 function submitForm(e) {
   e.preventDefault();
-  console.log(1234);
+
+
+  //Get all input values or DOM elements
+  let name = document.querySelector(".name").value;
+  let email = document.querySelector(".email").value;
+  let message = document.querySelector(".message").value;
+  console.log(name, email, message);
 };
